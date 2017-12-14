7988c09ef910458d01f8f912b12f3696940ce90c
maithao2206 <thaoptm2206@gmail.com>
2017-12-14 19:54:40 +0700

    create structure

--- js/app-bootstrap.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    window.app = window.app || {};
-    app.models = app.models || {};
-    app.views = app.views || {};
-
-}());
--- js/models/todo.js
+++
@@ -1,8 +0,0 @@
-app.models.Todo = function(text, id) {
-    this.id = id || new Date().valueOf();
-    this.text = text;
-    this.isComplete = false;
-}
-app.models.Todo.prototype.edit = function(text) {
-    this.text = text;
-}
